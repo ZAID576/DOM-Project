aa7391e45adaa6e5b83ee281d7100a26db7a11ae
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-07-27 23:57:16 +0530

    Background color changer

--- Project 1 [Background Color Changer]/script.js
+++ Project 1 [Background Color Changer]/script.js
@@ -1,26 +1,13 @@
-// // Step 1: Select all buttons
-// const buttons = document.querySelectorAll("button");
+let buttons = document.querySelectorAll("button");
 
-// // Step 2: Loop through buttons
-// buttons.forEach(button => {
-  
-//   // Step 3: Add click event
-//   button.addEventListener("click", () => {
-    
-//     // Step 4: Get color from data attribute
-//     const color = button.getAttribute("data-color");
+buttons.forEach((button) => {
 
-//     // Step 5: Change background color
-//     document.body.style.backgroundColor = color;
-//   });
+    button.addEventListener("click",() => {
 
-// });
+        let color = button.dataset.color;
 
-const buttons = document.querySelectorAll("button");
+        document.body.style.backgroundColor = color;
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const color = button.getAttribute("data-color");
-    document.body.style.backgroundColor = color;
-  });
+    });
+
 });
