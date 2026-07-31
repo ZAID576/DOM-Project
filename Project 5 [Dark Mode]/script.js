19f9528656bf1eafb2951ced4ff5134a276a0354
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-01 00:37:42 +0530

    Dark Mode

---
+++ Project 5 [Dark Mode]/script.js
@@ -0,0 +1,8 @@
+const themeBtn = document.querySelector("#themeBtn");
+const body = document.body
+
+themeBtn.addEventListener("click", ()=>{
+    body.classList.toggle("dark")
+    //  console.log("button clicked");
+     
+})
